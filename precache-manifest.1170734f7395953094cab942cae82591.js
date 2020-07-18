995929a8d7e87de6f582fad7f61a7b4d3359d7c7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-18 18:14:12 +0000

    Deploy ayarhlaine/react-recoiljs-facebook-clone to github.com/ayarhlaine/react-recoiljs-facebook-clone.git:gh-pages

---
+++ precache-manifest.1170734f7395953094cab942cae82591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4eb8792c557b784c53f15a34f66af6",
+    "url": "/react-recoiljs-facebook-clone/index.html"
+  },
+  {
+    "revision": "2c6ce016e9de188b3608",
+    "url": "/react-recoiljs-facebook-clone/static/css/main.bd947e63.chunk.css"
+  },
+  {
+    "revision": "838827e7444655039532",
+    "url": "/react-recoiljs-facebook-clone/static/js/2.d4107cb9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-recoiljs-facebook-clone/static/js/2.d4107cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6ce016e9de188b3608",
+    "url": "/react-recoiljs-facebook-clone/static/js/main.4d15c229.chunk.js"
+  },
+  {
+    "revision": "8cab53fb85c6c8add86b",
+    "url": "/react-recoiljs-facebook-clone/static/js/runtime-main.60b092ab.js"
+  }
+]);
